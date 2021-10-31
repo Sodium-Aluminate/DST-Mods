ea15abb3da52906fbecae5e3c6b80ee3d5b19f20
Sodium-Aluminate <NaAlOH4@NaAlOH4.com>
2021-10-31 23:55:07 +0800

    合并 mod

--- PoFix/modinfo.lua
+++ PoFix/modinfo.lua
@@ -2,14 +2,14 @@ local applied_lang = (locale == "en" or locale == "zhr") and "en" or "zh"
 
 
 local mname = {
-	zh = "瓦格斯塔夫工具名称显示",
-	en = "wagstaff tool names fix"
+	zh = "字符串替换",
+	en = "Po Fix"
 }
 name = mname[applied_lang]
 
 local mdescription = {
-	zh = "让你看得懂瓦格斯塔夫想你索要的工具名字。",
-	en = "let you understand which tool Wagstaff asking."
+	zh = "替换游戏内的一些字串。",
+	en = "Replace some strings in game."
 }
 description = mdescription[applied_lang]
 
@@ -18,7 +18,7 @@ author = "t.me/NaAlOH4"
 version = "1.0"
 
 api_version = 10
-priority = -9000
+-- priority = -9000
 dont_starve_compatible = false
 reign_of_giants_compatible = false
 shipwrecked_compatible = false
