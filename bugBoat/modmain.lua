cd275a864d5eddd8438eaa39ac9e9a2b7807cf42
Sodium-Aluminate <NaAlOH4@NaAlOH4.com>
2022-08-13 22:52:07 +0800

    使用mod接口来防止科雷改代码导致mod失效（

--- bugBoat/modmain.lua
+++ bugBoat/modmain.lua
@@ -1,2 +1,22 @@
 GLOBAL.TUNING.COM_NAALOH4_ALLOW_BUG_BOAT=(GetModConfigData("allowBugBoat") == 1)
 GLOBAL.TUNING.COM_NAALOH4_KEEP_BUG_BOAT=(GetModConfigData("keepBugBoat") == 1)
+
+AddStategraphPostInit("boat", function(inst_sg)
+    local oldPlaceFn = inst_sg.states.place.events.animover.fn
+    local oldIdleFn = inst_sg.states.idle.events.death.fn
+    inst_sg.states.place.events.animover.fn = function(inst_boat,data)
+        -- 如果开启了禁止无敌船，那么在船展开后重新给船回血来防止船死亡。
+        if (not TUNING.COM_NAALOH4_ALLOW_BUG_BOAT) then
+            inst_boat.components.health:SetPercent(1, false, nil)
+        end
+        return oldPlaceFn(inst_boat,data)
+    end
+
+    inst_sg.states.idle.events.death.fn = function(inst_boat, data)
+        -- 如果是是读档造成的死亡，且开启了保护船的功能，那么忽略这次死亡。
+        if (data and data.cause == "file_load" and TUNING.COM_NAALOH4_KEEP_BUG_BOAT) then
+            return;
+        end
+        return oldIdleFn(inst_boat,data)
+    end
+end)
