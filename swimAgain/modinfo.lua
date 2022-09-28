a578c20b888e593ae5067daf3c2e955973a3f83b
Sodium-Aluminate <NaAlOH4@NaAlOH4.com>
2022-09-28 13:45:17 +0800

    游泳mod写好准备测试

--- swimAgain/modinfo.lua
+++ swimAgain/modinfo.lua
@@ -1,6 +1,9 @@
 name = "swim again"
 description = [[
-科雷你坏事作尽.jpg
+《科雷你坏事作尽》
+科雷增加了制作物品时不能在溺水状态的限制，也就是玩家在卡海游泳时很多事情都干不了了。
+以下选项可以修改制作动作的进入条件。
+你可以通过修改服务端 TUNING.COM_NAALOH4_ALLOW_SWIM 和 TUNING.COM_NAALOH4_CRAFT_CONDITION 的值来临时性测试其他的配置。
 ]]
 author = "NaAlOH4"
 version = "1.0"
@@ -21,8 +24,8 @@ configuration_options = {
         label = "游泳配置",
         hover = "制作物品时是否检查当前溺水状态",
         options = {
-            { description = "禁止游泳", data = 0, hover = "与原版一样，如果在溺水状态则无法通过制作物品来打断" },
-            { description = "允许游泳", data = 1, hover = "与旧版一样，可以通过制作物品来防止自己溺水" }
+            { description = "禁止游泳 (true)", data = 0, hover = "与原版一样，如果在溺水状态则无法通过制作物品来打断。" },
+            { description = "允许游泳 (true)", data = 1, hover = "与旧版一样，可以通过制作物品来防止自己溺水。" }
         },
         default = 1,
     },
@@ -31,10 +34,10 @@ configuration_options = {
         label = "禁止解控",
         hover = "制作物品时检查当前状态吗？",
         options = {
-            { description = "默认", data = -1, hover = "使用原版的检测逻辑。" },
-            { description = "忽略", data = 0, hover = "除游泳外其他状态皆不检查，可以解除大部分控制" },
-            { description = "繁忙", data = 1, hover = "玩家繁忙状态时无法制作物品，溺水时也不能" },
-            { description = "激进", data = 2, hover = "玩家必须在立定或移动状态才能制作物品" },
+            { description = "默认 (\"NOT_HOOK\")", data = -1, hover = "使用原版的检测逻辑，开启此选项等价于关闭模组。" },
+            { description = "忽略 (\"DISABLE_CHECK\")", data = 0, hover = "除溺水外其他状态皆不检查，是否可以打断溺水由“游泳配置决定”。" },
+            { description = "繁忙 (\"CHECK_BUSY\")", data = 1, hover = "玩家繁忙状态时无法制作物品，当然包括溺水。" },
+            { description = "激进 (\"STRICT\")", data = 2, hover = "玩家必须在立定或移动状态才能制作物品，用于特别不希望客户端作弊的情况。" },
         },
         default = 0,
     }
--- swimAgain/modmain.lua
+++ swimAgain/modmain.lua
@@ -1,18 +1,31 @@
-error("这个 mod 还没有完成。this mod is unfinished")
+print("这个 mod 还在测试阶段，可能会炸。")
 
-TUNING = GLOBAL.TUNING
 setmetatable = GLOBAL.setmetatable
-type = GLOBAL.type
-ipairs = GLOBAL.ipairs
 assert = GLOBAL.assert
 
 TUNING.COM_NAALOH4_ALLOW_SWIM = (GetModConfigData("allowSwim") == 1) -- 1: can swim; 0: can't swim.
 
+local NOT_HOOK = "NOT_HOOK"
+local DISABLE_CHECK = "DISABLE_CHECK"
+local CHECK_BUSY = "CHECK_BUSY"
+local STRICT = "STRICT"
+
+local BUSY_TAGS = {"busy","drowning"} -- 其实带溺水标签的都带繁忙，但是谁知道科雷以后怎么写呢
+local STRICT_SG_TAGS = { "idle", "moving", "running" }
+
+local function checkSG(sg, tags)
+    for _, tag in ipairs(tags) do
+        if (sg:HasStateTag(tag)) then
+            return true
+        end
+    end
+    return false
+end
 TUNING.COM_NAALOH4_CRAFT_CONDITION = ({
-    [-1] = "NOT_HOOK", -- use original code
-    [0] = "DISABLE_CHECK", -- allow any craft
-    [1] = "CHECK_BUSY", -- disallow craft when busy
-    [2] = "STRICT", -- only allow craft when idle/moving/running
+    [-1] = NOT_HOOK, -- use original code
+    [0] = DISABLE_CHECK, -- allow any craft
+    [1] = CHECK_BUSY, -- disallow craft when busy
+    [2] = STRICT, -- only allow craft when idle/moving/running
 })[GetModConfigData("banBusy")]
 
 assert(TUNING.COM_NAALOH4_CRAFT_CONDITION)
@@ -26,53 +39,81 @@ path 可以是数组也可以是字符串，如果是字符串则视为是包含
 如 hookedBuilder = tableValueOverride(Builder, {"inst","sg","HasStateTag"}, hookedFn) 可以 hook 这个函数而不修改其他的东西。
 
 ]]
+local rawTableKey = "com.NaAlOH4.tableValueOverride.rawTable"
+local hookingPathKey = "com.NaAlOH4.tableValueOverride.hookingPath"
+local targetValueKey = "com.NaAlOH4.tableValueOverride.targetValue"
 local function tableValueOverride(rawTable, path, targetValue)
     local hookingPath = type(path) == "string" and { path } or path
 
-    local toReturn = setmetatable({}, {
-        ["com.NaAlOH4.tableValueOverride.rawTable"] = rawTable,
-        ["com.NaAlOH4.tableValueOverride.hookingPath"] = hookingPath,
-        ["com.NaAlOH4.tableValueOverride.targetValue"] = targetValue,
-        __index = function(inst, key)
+    local metatable = {
+        [rawTableKey] = rawTable,
+        [hookingPathKey] = hookingPath,
+        [targetValueKey] = targetValue,
+    }
+    function metatable:__index(key)
 
-            local currentRawTable = inst["com.NaAlOH4.tableValueOverride.rawTable"]
-            local currentHookingPath = inst["com.NaAlOH4.tableValueOverride.hookingPath"]
-            local currentTargetValue = inst["com.NaAlOH4.tableValueOverride.targetValue"]
-            if (#currentHookingPath == 0) then
-                return currentRawTable[key]
-            end
-            if (currentHookingPath[1] == key) then
-                if (#currentHookingPath == 1) then
-                    return currentTargetValue
-                else
-                    local shadow = {}
-                    for i, v in ipairs(currentRawTable) do
-                        if (i > 1) then
-                            shadow[i - 1] = v
-                        end
-                    end
-                    return tableValueOverride(currentRawTable[key], shadow, currentTargetValue)
-                end
-            end
+        local currentRawTable = self[rawTableKey]
+        local currentHookingPath = self[hookingPathKey]
+        local currentTargetValue = self[targetValueKey]
+        if (#currentHookingPath == 0) then
             return currentRawTable[key]
-        end,
-        __newindex = function(inst, key, value)
-            inst["com.NaAlOH4.tableValueOverride.rawTable"][key] = value
-        end,
-    })
+        end
+        if (currentHookingPath[1] == key) then
+            if (#currentHookingPath == 1) then
+                return currentTargetValue
+            else
+                local shadow = {}
+                for i, v in ipairs(currentRawTable) do
+                    if (i > 1) then
+                        shadow[i - 1] = v
+                    end
+                end
+                return tableValueOverride(currentRawTable[key], shadow, currentTargetValue)
+            end
+        end
+        return currentRawTable[key]
+    end
+
+    function metatable:__newindex(key, value)
+        self[rawTableKey][key] = value
+    end
+
+    return toReturn;
 end
 
 AddComponentPostInit("builder", function(Builder, instEntitiy)
-    oldMakeRecipeFn = Builder.MakeRecipe
+    local oldMakeRecipeFn = Builder.MakeRecipe
 
     function Builder.MakeRecipe(Builder, recipe, pt, rot, skin, onsuccess)
-        old_HasStateTag_fn = Builder.inst.sg.HasStateTag
-        local HookedBuilder = tableValueOverride(Builder, { "inst", "sg", "HasStateTag", function(sg, tagName)
-            if (tagName == "drowning") then
-
+        if (TUNING.COM_NAALOH4_CRAFT_CONDITION == NOT_HOOK) then
+            return oldMakeRecipeFn(Builder, recipe, pt, rot, skin, onsuccess)
+        end
+        if (TUNING.COM_NAALOH4_CRAFT_CONDITION == DISABLE_CHECK) then
+            local old_HasStateTag_fn = Builder.inst.sg.HasStateTag
+            local HookedBuilder = tableValueOverride(Builder, { "inst", "sg", "HasStateTag", function(sg, tagName)
+                if (tagName == "drowning") then
+                    return not TUNING.COM_NAALOH4_ALLOW_SWIM;
+                end
+                if (tagName == "busy") then
+                    -- 根据注释未来可能改为繁忙检测
+                    return false
+                end
+                return old_HasStateTag_fn(sg, tagName)
+            end })
+            return oldMakeRecipeFn(HookedBuilder, recipe, pt, rot, skin, onsuccess)
+        end
+        if (TUNING.COM_NAALOH4_CRAFT_CONDITION == CHECK_BUSY) then
+            if (checkSG(Builder.inst.sg, BUSY_TAGS)) then
+                return false
             end
-        end })
-        return oldMakeRecipeFn(HookedBuilder, recipe, pt, rot, skin, onsuccess)
+            return oldMakeRecipeFn(Builder, recipe, pt, rot, skin, onsuccess)
+        end
+        if (TUNING.COM_NAALOH4_CRAFT_CONDITION == STRICT) then
+            if (checkSG(Builder.inst.sg, STRICT_SG_TAGS)) then
+                return oldMakeRecipeFn(Builder, recipe, pt, rot, skin, onsuccess)
+            end
+            return false
+        end
     end
 
 end)
