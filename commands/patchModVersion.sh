14a8b584bc302537fb9d9291bb0be3437cc6d436
Sodium-Aluminate <NaAlOH4@NaAlOH4.com>
2022-12-19 22:45:04 +0800

    timestamp for test

---
+++ commands/patchModVersion.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## 将 modinfo 的最后加上此代码以便于调试的时候检查 mod 版本
+## local timestamp = "2006.01.02_15:04:05" --[[AUTO GENERATE BY SCRIPT]]
+## version = version .. ' (' .. timestamp .. ')' --[[AUTO GENERATE BY SCRIPT]]
+## description = timestamp .. "\n" .. description --[[AUTO GENERATE BY SCRIPT]]
+
+sed -i "/.*--\[\[AUTO GENERATE BY SCRIPT\]\]/d" modinfo.lua
+timestamp=$(date +%Y.%m.%d_%H:%M:%S)
+echo 'local timestamp = "'$timestamp'" --[[AUTO GENERATE BY SCRIPT]]' >> modinfo.lua
+echo 'version = version .. " (" .. timestamp .. ")" --[[AUTO GENERATE BY SCRIPT]]' >> modinfo.lua
+echo 'description = timestamp .. "\n" .. description --[[AUTO GENERATE BY SCRIPT]]' >> modinfo.lua
